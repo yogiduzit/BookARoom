a9ceee60db6e69ea7bef4c6ad6a81aa372afbfde
ethanlee07 <43010904+ethanlee07@users.noreply.github.com>
2020-10-01 11:40:53 -0700

    basic main-menu done (#4)
    * basic main-menu done
    
    * Added for loops and arrays for the main menu
    
    * Made a single function to add items to spinner
    
    * Fixed the addToSpinner function

---
+++ app/src/main/java/com/example/bookaroom/ViewBookings.java
@@ -0,0 +1,47 @@
+package com.example.bookaroom;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ViewBookings extends MainActivity {
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bookings);
+        String[] recreationalArr = getResources().getStringArray(R.array.recreational);
+        String[] libraryArr = getResources().getStringArray(R.array.library);
+        String[] downtownArr = getResources().getStringArray(R.array.downtown);
+        addItemsToSpinner(R.id.recreational_spinner, recreationalArr);
+        addItemsToSpinner(R.id.library_spinner, libraryArr);
+        addItemsToSpinner(R.id.downtown_spinner, downtownArr);
+    }
+    public void addItemsToSpinner(Integer id, String[] items){
+        spinner = findViewById(id);
+        List<String> list = new ArrayList<String>();
+        for(int i = 0; i < items.length;i++){
+            list.add(items[i]);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+        parent.getItemAtPosition(position);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent){
+
+    }
+}
