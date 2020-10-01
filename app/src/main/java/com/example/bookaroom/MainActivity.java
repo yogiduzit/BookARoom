acb7bd61bd8e4a60de2efdc5c3c783a0e19e44be
Yogesh Verma <yverma2@my.bcit.ca>
2020-10-01 11:17:03 -0700

    Implement functionality for browser links on the login page (#2)
    * Can open BCIT login using button
    
    * Improved styling and comments
    
    Pivot to constraint layout
    Added JavaDoc comments for functions

--- app/src/main/java/com/example/bookaroom/MainActivity.java
+++ app/src/main/java/com/example/bookaroom/MainActivity.java
@@ -2,9 +2,31 @@ package com.example.bookaroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BCIT_URL = "https://id.bcit.ca";
+    public static final String GO_FORWARD_URL = "https://www.bcit.ca/covid-19/return-to-operations/";
+
+    /**
+     * Authentication status of the user
+     */
+    private boolean loggedIn = false;
+
+    /**
+     * Stores the key value pairs containing the button id
+     * and link to be opened on clicking the button corresponding
+     * to the id
+     */
+    private Map<Integer, String> linksMap = new HashMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +34,55 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+        setupLinks();
+        setButtonTitle();
     }
+
+    /**
+     * Handles the click action on main button
+     * @param view the clicked element
+     */
+    public void onMainBtnClick(View view) {
+        openURL(view.getId());
+    }
+
+    /**
+     * Handles the click action on Go Forward link
+     * @param view the clicked element
+     */
+    public void onGoForwardClick(View view) {
+        openURL(view.getId());
+    }
+
+    /**
+     * Populates the links map with the key-value pairs
+     * containing the view id and links to be used on main page.
+     */
+    private void setupLinks() {
+        linksMap.put(R.id.mainBtn, MainActivity.BCIT_URL);
+        linksMap.put(R.id.go_forward_link, MainActivity.GO_FORWARD_URL);
+    }
+
+    /**
+     * Sets the button title for the main button depending
+     * upon the user authentication status
+     * If the user is logged in, set it to "View bookings"
+     * If the user isn't logged in, set it to "Login"
+     */
+    private void setButtonTitle() {
+        TextView mainBtn = (TextView) findViewById(R.id.mainBtn);
+        String title = getString(loggedIn ? R.string.view_bookings : R.string.login);
+        mainBtn.setText(title);
+    }
+
+    /**
+     * Opens a provided URL in external browser
+     * @param urlId the url to be opened
+     */
+    private void openURL(Integer urlId) {
+        String url = linksMap.get(urlId);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(launchBrowser);
+    }
+
 }
