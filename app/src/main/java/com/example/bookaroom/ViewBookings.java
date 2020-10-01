4ece78f3a12006c3612b083167f0ba4b8e16267a
ethanlee07 <elee270@my.bcit.ca>
2020-10-01 11:08:31 -0700

    Made a single function to add items to spinner

--- app/src/main/java/com/example/bookaroom/ViewBookings.java
+++ app/src/main/java/com/example/bookaroom/ViewBookings.java
@@ -1,5 +1,6 @@
 package com.example.bookaroom;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,63 +9,29 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 public class ViewBookings extends MainActivity {
-    private Spinner recreationalDropdown;
-    private Spinner libraryDropdown;
-    private Spinner downtownDropdown;
-    private String[] recreationalArr;
-    private String[] libraryArr;
-    private String[] downtownArr;
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_bookings);
-        addItemsToRecreationalSpinner();
-        addItemsToLibrarySpinner();
-        addItemsToDowntownSpinner();
+        addItemsToSpinner(R.id.recreational_spinner, getResources().getStringArray(R.array.recreational));
+        addItemsToSpinner(R.id.library_spinner, getResources().getStringArray(R.array.library));
+        addItemsToSpinner(R.id.downtown_spinner, getResources().getStringArray(R.array.downtown));
     }
-
-
-    public void addItemsToRecreationalSpinner(){
-        recreationalDropdown = (Spinner)findViewById(R.id.recreational_spinner);
+    public void addItemsToSpinner(Integer id, String[] items){
+        spinner = findViewById(id);
         List<String> list = new ArrayList<String>();
-        recreationalArr = getResources().getStringArray(R.array.recreational);
-        for(int i = 0; i < recreationalArr.length;i++){
-            list.add(recreationalArr[i]);
+        for(int i = 0; i < items.length;i++){
+            list.add(items[i]);
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        recreationalDropdown.setAdapter(adapter);
-    }
-
-    public void addItemsToLibrarySpinner(){
-        libraryDropdown = (Spinner)findViewById(R.id.library_spinner);
-        List<String> list = new ArrayList<String>();
-        libraryArr = getResources().getStringArray(R.array.library);
-        for (int i = 0; i < libraryArr.length;i++){
-            list.add(libraryArr[i]);
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        libraryDropdown.setAdapter(adapter);
-    }
-
-    public void addItemsToDowntownSpinner(){
-        downtownDropdown = (Spinner)findViewById(R.id.downtown_spinner);
-        List<String> list = new ArrayList<String>();
-        downtownArr = getResources().getStringArray(R.array.downtown);
-        for(int i = 0; i < downtownArr.length; i++){
-            list.add(downtownArr[i]);
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        downtownDropdown.setAdapter(adapter);
+        spinner.setAdapter(adapter);
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
