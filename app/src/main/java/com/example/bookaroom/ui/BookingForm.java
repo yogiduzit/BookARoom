3c56c2b42e6a851e4ab41843e31b934bad798dbc
Quang Minh Pham <minhpham10b@gmail.com>
2020-10-07 14:52:29 -0700

    Made Book button to move from booking form to booking confirmation

--- app/src/main/java/com/example/bookaroom/ui/BookingForm.java
+++ app/src/main/java/com/example/bookaroom/ui/BookingForm.java
@@ -13,9 +13,7 @@ public class BookingForm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_booking_form);
-
     }
 
     public void onBookingConfirmed(View view) {
