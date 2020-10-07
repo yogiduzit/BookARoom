b9eddf2ff9fb3031660ac264ed7cefff5c65d845
Yogesh Verma <yverma2@my.bcit.ca>
2020-10-07 15:30:01 -0700

    Booking form (#8)
    * Booking form and booking confirmation
    
    * Made Book button to move from booking form to booking confirmation
    
    * Removed library in gradle and set default activity back to MainActivity
    
    Co-authored-by: Quang Minh Pham <minhpham10b@gmail.com>

---
+++ app/src/main/java/com/example/bookaroom/BookingConfirmation.java
@@ -0,0 +1,15 @@
+package com.example.bookaroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_booking_confirmation);
+    }
+}
