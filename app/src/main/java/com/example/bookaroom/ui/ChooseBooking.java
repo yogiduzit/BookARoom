2c03e8dbf8543c092599801cc41373f8feff0691
yogiduzit <yverma2@my.bcit.ca>
2020-10-15 13:23:28 -0700

    Resolve merge conflicts

--- app/src/main/java/com/example/bookaroom/ui/ChooseBooking.java
+++ app/src/main/java/com/example/bookaroom/ui/ChooseBooking.java
@@ -15,7 +15,6 @@ public class ChooseBooking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_booking);
-        getSupportActionBar().hide();
 
         TableView bookingsTableView = findViewById(R.id.bookings_table_view);
         initTableView(bookingsTableView);
