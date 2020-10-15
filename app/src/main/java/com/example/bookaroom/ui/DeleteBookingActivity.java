8ff6cb2651e3a61cb59a9e3a63f3f09e22667223
yogiduzit <yverma2@my.bcit.ca>
2020-10-15 14:24:36 -0700

    Resolve merge conflicts

--- app/src/main/java/com/example/bookaroom/ui/DeleteBookingActivity.java
+++ app/src/main/java/com/example/bookaroom/ui/DeleteBookingActivity.java
@@ -13,7 +13,6 @@ public class DeleteBookingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_delete_booking);
     }
 
--- app/src/main/java/com/example/bookaroom/ui/MyBookingsActivity.java
+++ app/src/main/java/com/example/bookaroom/ui/MyBookingsActivity.java
@@ -13,7 +13,6 @@ public class MyBookingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_my_bookings);
     }
 
