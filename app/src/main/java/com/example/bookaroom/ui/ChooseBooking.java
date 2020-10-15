fb4d1ca0bf82c55957231e8072a9d75e34b8e9f9
yogiduzit <yverma2@my.bcit.ca>
2020-10-15 13:27:27 -0700

    Connect to booking details activity

--- app/src/main/java/com/example/bookaroom/ui/ChooseBooking.java
+++ app/src/main/java/com/example/bookaroom/ui/ChooseBooking.java
@@ -2,7 +2,9 @@ package com.example.bookaroom.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.evrencoskun.tableview.TableView;
 import com.example.bookaroom.R;
@@ -26,5 +28,10 @@ public class ChooseBooking extends AppCompatActivity {
         TableViewModel viewModel = new TableViewModel();
 
         adapter.setAllItems(viewModel.getColumnHeaderList(), viewModel.getRowHeaderList(), viewModel.getCellList());
+    }
+
+    public void onBookingSelected(View view) {
+        Intent bookingDetails = new Intent(this, BookingForm.class);
+        startActivity(bookingDetails);
     }
 }
