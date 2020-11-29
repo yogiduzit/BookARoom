84290c9096607660a41b170077989bda8dfec919
Quang Minh Pham <minhpham10b@gmail.com>
2020-11-28 22:06:53 -0800

    Validate data of AdminPanelActivity

--- app/src/main/java/com/example/bookaroom/ui/AdminPanelActivity.java
+++ app/src/main/java/com/example/bookaroom/ui/AdminPanelActivity.java
@@ -3,6 +3,7 @@ package com.example.bookaroom.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,17 +29,33 @@ public class AdminPanelActivity extends AppCompatActivity {
         dayEndTime = findViewById(R.id.day_end_input);
         baseBookingInterval = findViewById(R.id.base_interval_input);
         submitBtn = findViewById(R.id.submitButton);
+        final int[] ids = new int[]{R.id.day_start_input, R.id.day_end_input, R.id.base_interval_input};
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AdminPanelActivity.this, "Modified Admin Panel successfully!", Toast.LENGTH_LONG).show();
-                AdminPanel.DAY_START_TIME = Integer.parseInt(dayStartTime.getText().toString().trim());
-                AdminPanel.DAY_END_TIME = Integer.parseInt(dayEndTime.getText().toString().trim());
-                AdminPanel.BASE_BOOKING_INTERVAL = Double.parseDouble(baseBookingInterval.getText().toString().trim());
-                dayStartTime.setText("");
-                dayEndTime.setText("");
-                baseBookingInterval.setText("");
+                if (!validate(ids )) {
+                    Toast.makeText(AdminPanelActivity.this, "Modified Admin Panel successfully!", Toast.LENGTH_LONG).show();
+                    AdminPanel.DAY_START_TIME = Integer.parseInt(dayStartTime.getText().toString().trim());
+                    AdminPanel.DAY_END_TIME = Integer.parseInt(dayEndTime.getText().toString().trim());
+                    AdminPanel.BASE_BOOKING_INTERVAL = Double.parseDouble(baseBookingInterval.getText().toString().trim());
+                    dayStartTime.setText("");
+                    dayEndTime.setText("");
+                    baseBookingInterval.setText("");
+                }
+
             }
         });
+    }
+
+    public boolean validate(int[] ids){
+        boolean isEmpty = false;
+        for(int id: ids){
+            EditText editText = findViewById(id);
+            if(TextUtils.isEmpty(editText.getText().toString())){
+                editText.setError("Enter a Value");
+                isEmpty = true;
+            }
+        }
+        return isEmpty;
     }
 }
