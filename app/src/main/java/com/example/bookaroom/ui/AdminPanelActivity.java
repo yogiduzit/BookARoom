c1e49a3098cbdfde2fc79d1cce57d99a55770032
yogiduzit <yverma2@my.bcit.ca>
2020-11-29 13:43:05 -0800

    Improved Admin Panel

--- app/src/main/java/com/example/bookaroom/ui/AdminPanelActivity.java
+++ app/src/main/java/com/example/bookaroom/ui/AdminPanelActivity.java
@@ -2,22 +2,18 @@ package com.example.bookaroom.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.bookaroom.AdminPanel;
 import com.example.bookaroom.R;
+import com.example.bookaroom.helpers.ToastHelper;
 
 public class AdminPanelActivity extends AppCompatActivity {
-
-    public static final int DAY_START_TIME = 6;
-    public static final int DAY_END_TIME = 20;
-    public static final double BASE_BOOKING_INTERVAL = 0.5;
-
     Button submitBtn;
     EditText dayStartTime, dayEndTime, baseBookingInterval, maxBookingsPerRoom;
 
@@ -30,34 +26,43 @@ public class AdminPanelActivity extends AppCompatActivity {
         baseBookingInterval = findViewById(R.id.base_interval_input);
         maxBookingsPerRoom = findViewById(R.id.max_booking_per_room_input);
         submitBtn = findViewById(R.id.submitButton);
-        final int[] ids = new int[]{R.id.day_start_input, R.id.day_end_input, R.id.base_interval_input, R.id.max_booking_per_room_input};
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!validate(ids )) {
-                    Toast.makeText(AdminPanelActivity.this, "Modified Admin Panel successfully!", Toast.LENGTH_LONG).show();
-                    AdminPanel.DAY_START_TIME = Integer.parseInt(dayStartTime.getText().toString().trim());
-                    AdminPanel.DAY_END_TIME = Integer.parseInt(dayEndTime.getText().toString().trim());
-                    AdminPanel.BASE_BOOKING_INTERVAL = Double.parseDouble(baseBookingInterval.getText().toString().trim());
-                    AdminPanel.MAX_BOOKINGS_PER_ROOM = Integer.parseInt(maxBookingsPerRoom.getText().toString().trim());
-                    dayStartTime.setText("");
-                    dayEndTime.setText("");
-                    baseBookingInterval.setText("");
-                }
 
+        dayEndTime.setText(Integer.toString(AdminPanel.DAY_END_TIME));
+        dayStartTime.setText(Integer.toString(AdminPanel.DAY_START_TIME));
+        baseBookingInterval.setText(Double.toString(AdminPanel.BASE_BOOKING_INTERVAL));
+        maxBookingsPerRoom.setText(Integer.toString(AdminPanel.MAX_BOOKINGS_PER_ROOM));
+
+        final int[] ids = new int[]{R.id.day_start_input, R.id.day_end_input, R.id.base_interval_input, R.id.max_booking_per_room_input};
+        submitBtn.setOnClickListener(v -> {
+            if (!validate(ids)) {
+                ToastHelper.showToast(AdminPanelActivity.this, ToastHelper.Severity.SUCCESS, "Modified admin panel successfully", Toast.LENGTH_LONG);
+                AdminPanel.DAY_START_TIME = Integer.parseInt(dayStartTime.getText().toString().trim());
+                AdminPanel.DAY_END_TIME = Integer.parseInt(dayEndTime.getText().toString().trim());
+                AdminPanel.BASE_BOOKING_INTERVAL = Double.parseDouble(baseBookingInterval.getText().toString().trim());
+                AdminPanel.MAX_BOOKINGS_PER_ROOM = Integer.parseInt(maxBookingsPerRoom.getText().toString().trim());
+                dayStartTime.setText("");
+                dayEndTime.setText("");
+                baseBookingInterval.setText("");
+                maxBookingsPerRoom.setText("");
+                onAdminPanelEdited();
             }
         });
     }
 
-    public boolean validate(int[] ids){
+    public boolean validate(int[] ids) {
         boolean isEmpty = false;
-        for(int id: ids){
+        for (int id : ids) {
             EditText editText = findViewById(id);
-            if(TextUtils.isEmpty(editText.getText().toString())){
+            if (TextUtils.isEmpty(editText.getText().toString())) {
                 editText.setError("Enter a Value");
                 isEmpty = true;
             }
         }
         return isEmpty;
+    }
+
+    private void onAdminPanelEdited() {
+        Intent i = new Intent(this, ViewBookings.class);
+        startActivity(i);
     }
 }
